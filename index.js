0890855eb0658f7a65f1a9d89069904d36fc401c
Oskari Virtaoja <oskari@smartly.io>
2019-12-13 12:13:21 +0200

    Refactor index.js to use functions
    Instead of using comments to describe what the code does, refactor them
    inside functions describing the functionality.
    
    This way the code is more modular and easier to develop further.
    
    Also started to use js 'enum' to reference to different versions.

--- index.js
+++ index.js
@@ -1,42 +1,59 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const Versions = {
+  major: 'MAJOR',
+  minor: 'MINOR',
+  patch: 'PATCH',
+  unknown: ''
+}
+
+function fetch_labels() {
+  const label_array = github.context.payload.pull_request.labels.map(a => a.name);
+
+  return {
+    major: label_array.includes('major'),
+    minor: label_array.includes('minor'),
+    patch: label_array.includes('patch')
+  };
+}
+
+function warning_message() {
+  return `Invalid version specirifaction!
+Please specify one of the following tags:
+  major, minor, patch`;
+}
+
+function detect_version(labels) {
+  if (labels.major === true && labels.minor === false && labels.patch === false) {
+    return Versions.major;
+  } else if (labels.major === false && labels.minor === true && labels.patch === false) {
+    return Versions.minor;
+  } else if (lables.major === false && labels.minor === false && lables.patch === true) {
+    return Versions.minor;
+  } else {
+    return Versions.unknown;
+  }
+}
+
 try {
-  // Inputs
   const enforceSet = core.getInput('enforce');
 
-  // Predefining default outputs
-  let VERSION_UPPER = '';
-  let VERSION_LOWER = '';
+  const labels = fetch_labels()
+  const version = detect_version(labels)
 
-  //Let's get the list of labels
-  const label_array = github.context.payload.pull_request.labels.map(a => a.name);
-  const majorLabel = label_array.includes('major');
-  const minorLabel = label_array.includes('minor');
-  const patchLabel = label_array.includes('patch');
+  if (version === Versions.unknown) {
+    console.log(warning_message())
 
-  if (majorLabel === true && minorLabel === false && patchLabel === false) {
-    VERSION_UPPER = 'MAJOR';
-  } else if (majorLabel === false && minorLabel === true && patchLabel === false) {
-    VERSION_UPPER = 'MINOR';
-  } else if (majorLabel === false && minorLabel === false && patchLabel === true) {
-    VERSION_UPPER = 'PATCH';
-  } else {
-    console.log('Invalid version specification!');
-    console.log('Please specify one of the following tags:');
-    console.log('   major, minor, patch');
     if (enforceSet === 'true') {
       throw new Error('Invalid Verions specification');
     } else {
       console.log('NOTE: CURRENT STATE WILL NOT DO A RELEASE');
     }
   }
-  // Set the version lower as we'll need that too
-  VERSION_LOWER = VERSION_UPPER.toLowerCase();
-  // Return values for use in next step
-  core.setOutput('VERSION_UPPER', VERSION_UPPER);
-  core.setOutput('VERSION_LOWER', VERSION_LOWER);
 
+  core.setOutput('VERSION_UPPER', version.toUpperCase())
+  core.setOutput('VERSION_LOWER', version.toLowerCase())
 } catch (error) {
   core.setFailed(error.message);
 }
