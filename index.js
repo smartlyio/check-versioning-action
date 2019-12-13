d9432469a6f833df62a50901dfe2d42b1f65fb21
Rami Ertimo <rami.ertimo@smartly.io>
2019-12-13 14:08:46 +0200

    Minor refactoring for version checking

--- index.js
+++ index.js
@@ -1,59 +1,48 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-const Versions = {
-  major: 'MAJOR',
-  minor: 'MINOR',
-  patch: 'PATCH',
-  unknown: ''
-}
+const Versions = ['major', 'minor', 'patch'];
 
-function fetchLabels() {
-  const label_array = github.context.payload.pull_request.labels.map(a => a.name);
+function fetchAndFilterLabels() {
+  const labels = github.context.payload.pull_request.labels.filter(
+    (label) =>
+      Versions.includes(label.toLowerCase())
+  );
 
-  return {
-    major: label_array.includes('major'),
-    minor: label_array.includes('minor'),
-    patch: label_array.includes('patch')
-  };
+  return labels;
 }
 
 function warningMessage() {
-  return `Invalid version specirifaction!
+  return `Invalid version specification!
 Please specify one of the following tags:
   major, minor, patch`;
-}
-
-function detectVersion(labels) {
-  if (labels.major === true && labels.minor === false && labels.patch === false) {
-    return Versions.major;
-  } else if (labels.major === false && labels.minor === true && labels.patch === false) {
-    return Versions.minor;
-  } else if (lables.major === false && labels.minor === false && lables.patch === true) {
-    return Versions.minor;
-  } else {
-    return Versions.unknown;
-  }
 }
 
 try {
   const enforceSet = core.getInput('enforce');
 
-  const labels = fetchLabels()
-  const version = detectVersion(labels)
+  let versionLabels = fetchAndFilterLabels();
 
-  if (version === Versions.unknown) {
-    console.log(warningMessage())
+  let version = ''; // A version need to be give as output
+
+  // No version or too many versions found
+  if (versionLabels.length !== 1) {
+    console.log(warningMessage());
 
     if (enforceSet === 'true') {
-      throw new Error('Invalid Verions specification');
+      throw new Error('Invalid version specification');
     } else {
       console.log('NOTE: CURRENT STATE WILL NOT DO A RELEASE');
     }
   }
+  // Just one valid version
+  else {
+    version = versionLabels[0];
+  }
 
-  core.setOutput('VERSION_UPPER', version.toUpperCase())
-  core.setOutput('VERSION_LOWER', version.toLowerCase())
+  core.setOutput('VERSION_UPPER', version.toUpperCase());
+  core.setOutput('VERSION_LOWER', version.toLowerCase());
+
 } catch (error) {
   core.setFailed(error.message);
 }
