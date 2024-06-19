ab35ee9ee8394c5bff0aed86c118f6e302ebcacf
Antti Harju <antti.harju@smartly.io>
2024-06-19 17:14:46 +0300

    Add support for push action (#469)
    * Run npm audit fix
    
    * Add support for push actions

--- src/fetchLabels.ts
+++ src/fetchLabels.ts
@@ -1,4 +1,5 @@
 /* eslint-disable */
+import * as core from "@actions/core";
 import * as github from "@actions/github";
 
 type Label = {
@@ -7,15 +8,49 @@ type Label = {
 
 export default async function fetchLabels(
   token: string,
-  pullRequest: any
+  context: any
 ): Promise<Label[]> {
   const client = github.getOctokit(token);
 
-  const PRPayload = await client.rest.pulls.get({
-    owner: pullRequest.owner,
-    repo: pullRequest.repo,
-    pull_number: pullRequest.number,
-  });
-  const allLabels: Label[] = PRPayload.data.labels as Label[];
+  const payload = await getPayload(client, context);
+  const allLabels: Label[] = payload.data.labels as Label[];
   return allLabels;
+}
+
+async function getPayload(client: any, context: any) {
+  if (context.eventName === "push") {
+    return await client.rest.pulls.get({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      pull_number: await getPullNumber(client, context),
+    });
+  }
+  return await client.rest.pulls.get({
+    owner: context.issue.owner,
+    repo: context.issue.repo,
+    pull_number: context.issue.number,
+  });
+}
+
+async function getPullNumber(client: any, context: any) {
+  const pullRequests =
+    await client.rest.repos.listPullRequestsAssociatedWithCommit({
+      commit_sha: context.sha,
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+    });
+
+  if (pullRequests.data.length !== 1) {
+    // Create a merge commit: sha corresponds to the merge commit sha
+    // Squash and merge: sha corresponds to the new commit sha
+    // Rebase and merge: surprisingly, rebasing also generates a new commit sha even if branch was up-to-date with master.
+    //
+    // None of these should be associated with any PR (unless there's a hash collision).
+    core.warning(
+      `Commit SHA (${context.sha}) is associated with multiple PRs! Did a hash collision occur?`
+    );
+    throw new Error("Ambiguous commit sha");
+  }
+
+  return pullRequests.data[0].number;
 }
