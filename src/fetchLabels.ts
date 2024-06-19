b15a9fb1562b56dac865fd6e749a9da392fffb0c
Antti Harju <antti.harju@smartly.io>
2024-06-19 16:38:39 +0300

    Fix formatting

--- src/fetchLabels.ts
+++ src/fetchLabels.ts
@@ -40,7 +40,7 @@ async function getPullNumber(client: any, context: any) {
       repo: context.repo.repo,
     });
 
-  core.info(pullRequests.data)
+  core.info(pullRequests.data);
   if (pullRequests.data.length !== 1) {
     core.warning(
       `Multiple PRs associated with the current commit sha! Please provide a unique sha (avoid cherry-picking and creating branches off of branches).`
