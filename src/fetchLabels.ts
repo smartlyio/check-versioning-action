4fb1f0ea5da47bd2067328e45b4d8349634168ea
Antti Harju <antti.harju@smartly.io>
2024-06-19 10:47:30 +0300

    Add support for push actions

--- src/fetchLabels.ts
+++ src/fetchLabels.ts
@@ -11,11 +11,32 @@ export default async function fetchLabels(
 ): Promise<Label[]> {
   const client = github.getOctokit(token);
 
-  const payload = await client.rest.pulls.get({
+  const payload = await getPayload(client, context);
+  const allLabels: Label[] = payload.data.labels as Label[];
+  return allLabels;
+}
+
+async function getPayload(client: any, context: any) {
+  if (context.eventName === "push") {
+    return await client.rest.pulls.get({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      pull_number: await getPullNumber(client, context),
+    });
+  }
+  return await client.rest.pulls.get({
     owner: context.issue.owner,
     repo: context.issue.repo,
     pull_number: context.issue.number,
   });
-  const allLabels: Label[] = payload.data.labels as Label[];
-  return allLabels;
+}
+
+async function getPullNumber(client: any, context: any) {
+  return (
+    await client.rest.repos.listPullRequestsAssociatedWithCommit({
+      commit_sha: context.sha,
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+    })
+  ).data[0].number;
 }
